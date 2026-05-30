ccec672bda4eff37181e5c312b59152a102499f5
Vinicius Eduardo Agnes <vinicius.agnes@v4company.com>
2026-05-30 20:02:22 -0300

    Test sitemap update

--- src/services/system-sitemap.js
+++ src/services/system-sitemap.js
@@ -1,3 +1,5 @@
 (function(){
-  if (window.V4_BOOT_LOG) window.V4_BOOT_LOG('sitemap', 'Sitemap loaded.');
+  const TABS=['Painel de Controle','Visao Geral','Plano de Acao','Tasks Atuais','Painel Central','Midia e Ads','CRM e Funil','Concorrentes SEMrush','Metas','Status do Projeto','Config do Cliente'];
+  window.V4_SYSTEM_SITEMAP={tabs:TABS};
+  if(window.V4_BOOT_LOG)window.V4_BOOT_LOG('sitemap','Sitemap data loaded.');
 })();
