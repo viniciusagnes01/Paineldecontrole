687b6bd6b95634d07c921938328b35962b025b96
Vinicius Eduardo Agnes <vinicius.agnes@v4company.com>
2026-05-30 01:18:18 -0300

    Bind eKyte workspace and project ids per client

--- src/data/clients-ekyte-config.js
+++ src/data/clients-ekyte-config.js
@@ -1,162 +1,201 @@
-// Configuração oficial Ekyte com todos os clientes
-window.V4_EKYTE_CLIENTS = [
-  {
-    id: 'alphaville',
-    name: 'Alphaville',
-    idWorkspaceEkyte: 126365,
-    idProjetoEkyte: 291862,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363257134796140',
-    nicho: 'Fechamento de sacadas',
-    palavrasChaveDrive: ['Alphaville', 'fechamento de sacadas', 'sacadas'],
-    pastaIconeFolderId: '1lG21qv4S7LCWhHe7gBw73NPlDSJ4m-yi',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/1_ED9-b-2-Cmslny5F4gPckhDEBXeboZt/edit?gid=1435070679'
-  },
-  {
-    id: 'yousafer',
-    name: 'YouSafer',
-    idWorkspaceEkyte: 72535,
-    idProjetoEkyte: 291857,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363299569409896',
-    nicho: 'SVA de saúde para provedores',
-    palavrasChaveDrive: ['YouSafer', 'SVA', 'provedores', 'Abramulti'],
-    pastaIconeFolderId: '1Iz2nt_MwESsFCeAZD6z9IjQ8fz45iRmY',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/1C-y6Rk68PB27BIZJnxv3Q7U8Cl7pjCfv/edit?gid=1736771800'
-  },
-  {
-    id: 'prime',
-    name: 'Prime',
-    idWorkspaceEkyte: 106839,
-    idProjetoEkyte: 291839,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363418609215409',
-    nicho: 'Oficina mecânica',
-    palavrasChaveDrive: ['Prime Mecânica', 'oficina', 'freios', 'automotivo'],
-    pastaIconeFolderId: '1ynLKciynIzr7gVtgiq3fy5IoTranCFAn',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/13Wih2KnUuqUN52jWELLK9Li6vg8KUXv6/edit?gid=182083209'
-  },
-  {
-    id: 'multimed',
-    name: 'MultiMed',
-    idWorkspaceEkyte: 124293,
-    idProjetoEkyte: 287299,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363423606250960',
-    nicho: 'Laboratório e exames',
-    palavrasChaveDrive: ['MultiMed', 'exames', 'laboratório', 'saúde'],
-    pastaIconeFolderId: '1H5kekxtbt-67S4K_ZB9ip_Qag6h9GupB',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/13MUedd1CFT2kKw7QJiP2I1K8kN6LBgZQ/edit?gid=657733996'
-  },
-  {
-    id: 'seg-eletronic',
-    name: 'Seg Eletronic',
-    idWorkspaceEkyte: 125773,
-    idProjetoEkyte: 273483,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363427075801557',
-    nicho: 'Segurança eletrônica',
-    palavrasChaveDrive: ['Seg Eletronic', 'câmera', 'segurança', 'diagnóstico'],
-    pastaIconeFolderId: '1naqEp5-RMWz7XEvsl50T5jpG2ecNDMDW',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/14Ll_RBSdAZxJIlTDCEQSrn3dytYrFi_x/edit?dls=true'
-  },
-  {
-    id: 'espaco-master',
-    name: 'Espaço Master',
-    idWorkspaceEkyte: 131114,
-    idProjetoEkyte: 276870,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363424198629431',
-    nicho: 'Residencial sênior',
-    palavrasChaveDrive: ['Espaço Master', 'idosos', 'residencial sênior'],
-    pastaIconeFolderId: '1tQgluKulSRjbMB6p0iZbUQ4x6UsDeiCC',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/1GU8tVEzA9sSMToJDlhnzp0rTQqWQb_1E/edit?gid=545398780'
-  },
-  {
-    id: 'st1-internet',
-    name: 'ST1 Internet',
-    idWorkspaceEkyte: 127063,
-    idProjetoEkyte: 273637,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363403300629023',
-    nicho: 'Provedor de internet',
-    palavrasChaveDrive: ['ST1 Internet', 'internet', 'telecom'],
-    pastaIconeFolderId: '1IOElrGUmuVZ37Rqr443lGHdKiJIuVMxw',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/1b2WIFOrC5cCP0npwqAXynnyt8Zd9zDxp/edit?gid=662502667'
-  },
-  {
-    id: 'sindi-hoteleiros',
-    name: 'SindiHoteleiros',
-    idWorkspaceEkyte: 87694,
-    idProjetoEkyte: 165830,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363372501205454',
-    nicho: 'Sindicato + benefício de saúde',
-    palavrasChaveDrive: ['SindiHoteleiros', 'Cuidar On', 'hotéis', 'saúde'],
-    pastaIconeFolderId: '17q8l0y5OhxL5qBtbv3RzyDUUVSL9nAPy',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/1Hqwzj_OULGIinyK9U78QMHX81w_GkHoo/edit?rtpof=true'
-  },
-  {
-    id: 'treinando-online',
-    name: 'Treinando Online',
-    idWorkspaceEkyte: null,
-    idProjetoEkyte: null,
-    idTipoTarefaEkyte: 55820,
-    idTaskModeloEkyte: 8851196,
-    executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
-    groupId: '120363421384631664',
-    nicho: 'Cursos e infoproduto',
-    palavrasChaveDrive: ['Treinando Online', 'curso', 'mentor', 'educação'],
-    pastaIconeFolderId: '1Ofe7NY2WooeZFTWvH3zmeK8W1AaEAl9n',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/d/16Lwrk9UT6f86xGAyH4i6Sfw33pqVAN16eKJVl5Izhbs'
-  },
-  {
-    id: 'black-ops',
-    name: 'BLACK OPS',
-    type: 'admin',
-    descricao: 'Painel geral de controle de todos os clientes',
-    idWorkspaceEkyte: null,
-    idProjetoEkyte: null,
-    groupId: 'admin-001',
-    planilhaOficial: 'https://docs.google.com/spreadsheets/u/1/d/1ET6cmm3SHCO_DnxJwMTLrDFpQ-pMOcSUm56hY5ZnGmk/edit?gid=744728561'
+// Configuracao oficial eKyte por cliente
+(function () {
+  const STORAGE_KEY = 'v4-command-center-state-v6-crm-performance-losses';
+
+  window.V4_EKYTE_CLIENTS = [
+    {
+      id: 'alphaville',
+      name: 'Alphaville',
+      idWorkspaceEkyte: 126365,
+      idProjetoEkyte: 291862,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363257134796140',
+      nicho: 'Fechamento de sacadas',
+      palavrasChaveDrive: ['Alphaville', 'fechamento de sacadas', 'sacadas'],
+      pastaIconeFolderId: '1lG21qv4S7LCWhHe7gBw73NPlDSJ4m-yi',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1CtfFiB0q2B72Cwb9VjTkv8gZHYtBv2keICwBNj7Z0ws/edit?gid=833926654',
+      ekyte: { companyId: '7773', workspaceId: 126365, projectId: 291862, taskScope: 'project' }
+    },
+    {
+      id: 'yousafer',
+      name: 'YouSafer',
+      idWorkspaceEkyte: 72535,
+      idProjetoEkyte: 291857,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363299569409896',
+      nicho: 'SVA de saúde para provedores',
+      palavrasChaveDrive: ['YouSafer', 'SVA', 'provedores', 'Abramulti'],
+      pastaIconeFolderId: '1Iz2nt_MwESsFCeAZD6z9IjQ8fz45iRmY',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1KLxctUK2ZGaM7jm1y2zj-StwLTgV6qP0PL1a-ZEnMmo/edit?gid=316084875',
+      ekyte: { companyId: '7773', workspaceId: 72535, projectId: 291857, taskScope: 'project' }
+    },
+    {
+      id: 'prime',
+      name: 'Prime',
+      idWorkspaceEkyte: 106839,
+      idProjetoEkyte: 291839,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363418609215409',
+      nicho: 'Oficina mecânica',
+      palavrasChaveDrive: ['Prime Mecânica', 'oficina', 'freios', 'automotivo'],
+      pastaIconeFolderId: '1ynLKciynIzr7gVtgiq3fy5IoTranCFAn',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1h6-xdgyekZrNLZ4luZU61S0hzh0Z-HLAZR7qCm8NQG8/edit?gid=1986904416',
+      ekyte: { companyId: '7773', workspaceId: 106839, projectId: 291839, taskScope: 'project' }
+    },
+    {
+      id: 'multimed',
+      name: 'MultiMed',
+      idWorkspaceEkyte: 124293,
+      idProjetoEkyte: 287299,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363423606250960',
+      nicho: 'Laboratório e exames',
+      palavrasChaveDrive: ['MultiMed', 'exames', 'laboratório', 'saúde'],
+      pastaIconeFolderId: '1H5kekxtbt-67S4K_ZB9ip_Qag6h9GupB',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1h4obelICw7z1rbYNaEdkbrCH-qYUhFttzW3SopNxodg/edit',
+      ekyte: { companyId: '7773', workspaceId: 124293, projectId: 287299, taskScope: 'project' }
+    },
+    {
+      id: 'seg-eletronic',
+      name: 'Seg Eletronic',
+      idWorkspaceEkyte: 125773,
+      idProjetoEkyte: 273483,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363427075801557',
+      nicho: 'Segurança eletrônica',
+      palavrasChaveDrive: ['Seg Eletronic', 'câmera', 'segurança', 'diagnóstico'],
+      pastaIconeFolderId: '1naqEp5-RMWz7XEvsl50T5jpG2ecNDMDW',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1-CSmqLVLbfwVuVxkez4Q38fSTkVzyGOudUmj_GIxHAc/edit?gid=1929982003',
+      ekyte: { companyId: '7773', workspaceId: 125773, projectId: 273483, taskScope: 'project' }
+    },
+    {
+      id: 'espaco-master',
+      name: 'Espaço Master',
+      idWorkspaceEkyte: 131114,
+      idProjetoEkyte: 276870,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363424198629431',
+      nicho: 'Residencial sênior',
+      palavrasChaveDrive: ['Espaço Master', 'idosos', 'residencial sênior'],
+      pastaIconeFolderId: '1tQgluKulSRjbMB6p0iZbUQ4x6UsDeiCC',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/19-VWUfoJD27KxlFEn8uvoDVTKkahT7CSLlVoZD5-VUQ/edit?gid=1733941213',
+      ekyte: { companyId: '7773', workspaceId: 131114, projectId: 276870, taskScope: 'project' }
+    },
+    {
+      id: 'st1-internet',
+      name: 'ST1 Internet',
+      idWorkspaceEkyte: 127063,
+      idProjetoEkyte: 273637,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363403300629023',
+      nicho: 'Provedor de internet',
+      palavrasChaveDrive: ['ST1 Internet', 'internet', 'telecom'],
+      pastaIconeFolderId: '1IOElrGUmuVZ37Rqr443lGHdKiJIuVMxw',
+      planilhaOficial: 'https://docs.google.com/spreadsheets/d/1BurqRDqYbWq8dPVxXiKjWH6WmfBNoe39AymwJM8LpFA/edit?gid=1699545222',
+      ekyte: { companyId: '7773', workspaceId: 127063, projectId: 273637, taskScope: 'project' }
+    },
+    {
+      id: 'sindihoteleiros-cuidar-on',
+      aliases: ['sindi-hoteleiros', 'sindihoteleiros'],
+      name: 'SindiHoteleiros',
+      idWorkspaceEkyte: 87694,
+      idProjetoEkyte: 165830,
+      idTipoTarefaEkyte: 55820,
+      idTaskModeloEkyte: 8851196,
+      executorIdEkyte: '782e3f64-e027-4eff-8d6c-716cf76c1532',
+      groupId: '120363372501205454',
+      nicho: 'Sindicato + benefício de saúde',
+      palavrasChaveDrive: ['SindiHoteleiros', 'Cuidar On', 'hotéis', 'saúde'],
+      pastaIconeFolderId: '17q8l0y5OhxL5qBtbv3RzyDUUVSL9nAPy',
+      planilhaOficial: '',
+      ekyte: { companyId: '7773', workspaceId: 87694, projectId: 165830, taskScope: 'project' }
+    }
+  ];
+
+  window.V4_EKYTE_CLIENT_BINDINGS = window.V4_EKYTE_CLIENTS.reduce((acc, client) => {
+    acc[client.id] = client;
+    (client.aliases || []).forEach((alias) => { acc[alias] = client; });
+    return acc;
+  }, {});
+
+  window.V4_DRIVE_BRANDING_CONFIG = {
+    alphaville: { folderId: '1lG21qv4S7LCWhHe7gBw73NPlDSJ4m-yi', imagemPrincipal: 'logo' },
+    yousafer: { folderId: '1Iz2nt_MwESsFCeAZD6z9IjQ8fz45iRmY', imagemPrincipal: 'logo' },
+    prime: { folderId: '1ynLKciynIzr7gVtgiq3fy5IoTranCFAn', imagemPrincipal: 'logo' },
+    multimed: { folderId: '1H5kekxtbt-67S4K_ZB9ip_Qag6h9GupB', imagemPrincipal: 'logo' },
+    'seg-eletronic': { folderId: '1naqEp5-RMWz7XEvsl50T5jpG2ecNDMDW', imagemPrincipal: 'logo' },
+    'espaco-master': { folderId: '1tQgluKulSRjbMB6p0iZbUQ4x6UsDeiCC', imagemPrincipal: 'logo' },
+    'st1-internet': { folderId: '1IOElrGUmuVZ37Rqr443lGHdKiJIuVMxw', imagemPrincipal: 'logo' },
+    'sindihoteleiros-cuidar-on': { folderId: '17q8l0y5OhxL5qBtbv3RzyDUUVSL9nAPy', imagemPrincipal: 'logo' }
+  };
+
+  function getEkyteConfig(clientId) {
+    return window.V4_EKYTE_CLIENT_BINDINGS?.[clientId] || null;
   }
-];
 
-// Mapping para Drive branding
-window.V4_DRIVE_BRANDING_CONFIG = {
-  alphaville: { folderId: '1lG21qv4S7LCWhHe7gBw73NPlDSJ4m-yi', imagemPrincipal: 'logo' },
-  yousafer: { folderId: '1Iz2nt_MwESsFCeAZD6z9IjQ8fz45iRmY', imagemPrincipal: 'logo' },
-  prime: { folderId: '1ynLKciynIzr7gVtgiq3fy5IoTranCFAn', imagemPrincipal: 'logo' },
-  multimed: { folderId: '1H5kekxtbt-67S4K_ZB9ip_Qag6h9GupB', imagemPrincipal: 'logo' },
-  'seg-eletronic': { folderId: '1naqEp5-RMWz7XEvsl50T5jpG2ecNDMDW', imagemPrincipal: 'logo' },
-  'espaco-master': { folderId: '1tQgluKulSRjbMB6p0iZbUQ4x6UsDeiCC', imagemPrincipal: 'logo' },
-  'st1-internet': { folderId: '1IOElrGUmuVZ37Rqr443lGHdKiJIuVMxw', imagemPrincipal: 'logo' },
-  'sindi-hoteleiros': { folderId: '17q8l0y5OhxL5qBtbv3RzyDUUVSL9nAPy', imagemPrincipal: 'logo' },
-  'treinando-online': { folderId: '1Ofe7NY2WooeZFTWvH3zmeK8W1AaEAl9n', imagemPrincipal: 'logo' }
-};
+  function getDriveBrandingConfig(clientId) {
+    return window.V4_DRIVE_BRANDING_CONFIG?.[clientId] || null;
+  }
 
-// Função para obter configuração Ekyte do cliente
-function getEkyteConfig(clientId) {
-  return window.V4_EKYTE_CLIENTS?.find(c => c.id === clientId) || null;
-}
+  function decorateClient(client) {
+    const binding = getEkyteConfig(client?.id);
+    if (!binding) return client;
+    client.ekyte = {
+      ...(client.ekyte || {}),
+      companyId: binding.ekyte.companyId,
+      workspaceId: binding.idWorkspaceEkyte,
+      projectId: binding.idProjetoEkyte,
+      taskScope: 'project',
+      status: 'Projeto eKyte vinculado'
+    };
+    client.idWorkspaceEkyte = binding.idWorkspaceEkyte;
+    client.idProjetoEkyte = binding.idProjetoEkyte;
+    client.integrations = {
+      ...(client.integrations || {}),
+      ekyte: {
+        companyId: binding.ekyte.companyId,
+        workspaceId: binding.idWorkspaceEkyte,
+        projectId: binding.idProjetoEkyte,
+        status: 'Vinculado por cliente/projeto'
+      }
+    };
+    return client;
+  }
 
-// Função para obter Drive branding do cliente
-function getDriveBrandingConfig(clientId) {
-  return window.V4_DRIVE_BRANDING_CONFIG?.[clientId] || null;
-}
+  function decorateState(state) {
+    if (!state || !Array.isArray(state.clients)) return state;
+    state.clients.forEach(decorateClient);
+    state.settings = {
+      ...(state.settings || {}),
+      ekyteTaskScope: 'client_project',
+      ekyteBindingsVersion: '20260530-01'
+    };
+    return state;
+  }
+
+  try {
+    if (window.V4_SEED) decorateState(window.V4_SEED);
+    if (Array.isArray(window.V4_OFFICIAL_ACTIVE_CLIENTS)) window.V4_OFFICIAL_ACTIVE_CLIENTS.forEach(decorateClient);
+    const raw = localStorage.getItem(STORAGE_KEY);
+    if (raw) localStorage.setItem(STORAGE_KEY, JSON.stringify(decorateState(JSON.parse(raw))));
+  } catch (error) {
+    console.warn('[EkyteClientConfig]', error);
+  }
+
+  window.getEkyteConfig = getEkyteConfig;
+  window.getDriveBrandingConfig = getDriveBrandingConfig;
+})();
